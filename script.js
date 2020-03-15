92d658156d034c10fa7830f52c2179f295cbf6ce
andykingtv <60576760+andykingtv@users.noreply.github.com>
2020-03-15 12:13:15 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+//question 1
+var petArray = ["cat", "cow", "dog"];
+
+//question 2
+var petArray = ["cat", "cow", "dog"];
+console.log(petArray[1]);
+
+//question 3
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+console.log(petArray);
+
+//question 4
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+console.log(petArray);
+console.log(petArray.length);
+
+//question 5
+var catObject = {
+  name: "Goku",
+  colour: "Black",
+  age: 2
+};
+
+//question 6
+var catObject = {
+  name: "Goku",
+  colour: "Black",
+  age: 2
+};
+console.log(catObject.age);
+
+//question 7
+var catArray = [
+  {
+    name: "Goku",
+    colour: "Black",
+    age: 2
+  }
+];
+console.log(catArray);
+
+//question 8
+var catArray = [
+  {
+    name: "Goku",
+    colour: "Black",
+    age: 2
+  }
+];
+console.log(catArray);
+
+//question 9
+function logToConsole(theConsole) {
+  console.log(theConsole);
+}
+
+//question 10
+function logToConsole(theConsole) {
+  console.log(theConsole);
+}
+
+logToConsole("donkey");
